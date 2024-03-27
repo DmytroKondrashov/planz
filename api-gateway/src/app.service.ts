0e87dc51d8efe0fe2794aad731897cb931875b8f
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-27 20:04:46 +0200

    save current state of app resvice

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { LoginEvent } from './login.event.dto';
+import { Observable } from 'rxjs';
+import { Consumer, Producer } from 'kafkajs';
 
 @Injectable()
 export class AppService {
@@ -8,14 +10,49 @@ export class AppService {
     @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
   ) {}
 
-  getHello() {
-    this.authClient.emit('login_requiest', new LoginEvent('test', 'test'));
-  }
+  private kafkaProducer: Producer;
+  private kafkaConsumer: Consumer;
 
-  // createOrder({ userId, price }: CreateOrderRequest) {
-  //   this.billingClient.emit(
-  //     'order_created',
-  //     new OrderCreatedEvent('123', userId, price),
-  //   );
-  // }
+  async getHello() {
+    // This will work, but it will return the event instead of event data.
+    // return new Observable((observer) => {
+    //   this.authClient
+    //     .emit('login_request', new LoginEvent('test', 'test'))
+    //     .subscribe((response) => {
+    //       console.log('Received response from auth microservice:', response);
+    //       observer.next(response);
+    //       observer.complete();
+    //     });
+    // });
+
+
+
+    // This will not work
+    // Publish message to Kafka topic
+    // const kafkaMessage = { key: 'request_key', value: JSON.stringify('') };
+    // await this.kafkaProducer.send({
+    //   topic: 'microservice_topic',
+    //   messages: [kafkaMessage],
+    // });
+
+    // // Return a Promise that resolves when response is received
+    // return new Promise((resolve, reject) => {
+    //   // Subscribe to Kafka topic for response
+    //   const subscription =
+    //     this.kafkaConsumer.subscribeToResponseOf('microservice_topic');
+
+    //   // Handle response when received
+    //   subscription.subscribe({
+    //     next: (message) => {
+    //       const responseData = JSON.parse(message.value.toString());
+    //       resolve(responseData);
+    //       subscription.unsubscribe(); // Unsubscribe after receiving response
+    //     },
+    //     error: (error) => {
+    //       reject(error);
+    //       subscription.unsubscribe(); // Unsubscribe on error
+    //     },
+    //   });
+    // });
+  }
 }
