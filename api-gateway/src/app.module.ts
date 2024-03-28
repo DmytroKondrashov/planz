d55c69bc86100480ce4c70bf11cbeda70610a92c
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-28 20:13:21 +0200

    add mongoose module import

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { UsersModule } from './users/users.module';
         },
       },
     ]),
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
     AuthModule,
     UsersModule,
   ],
