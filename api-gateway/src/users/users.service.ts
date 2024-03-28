5cb7a84fcac2d3a15af6252915e0968dccad4fb5
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-28 11:27:28 +0200

    add create action

--- api-gateway/src/users/users.service.ts
+++ api-gateway/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './chemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = await this.userModel.create(createUserDto);
+    return createdUser;
+  }
+}
