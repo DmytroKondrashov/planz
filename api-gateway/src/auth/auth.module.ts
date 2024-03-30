0b1b6cf3b31ade0e763562365281f52310d83eae
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-30 14:03:15 +0200

    import JwtModule

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -4,11 +4,16 @@ import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/common/schemas/user.schema';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UsersService, JwtService],
