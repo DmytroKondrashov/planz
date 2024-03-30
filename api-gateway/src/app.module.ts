2bab3a095baaa07c1ae1f8181a24f3d7a6c281b1
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-30 11:16:34 +0200

    update imports

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from './users/users.service';
+import { User, UserSchema } from './common/schemas/user.schema';
 
 @Module({
   imports: [
@@ -26,10 +28,11 @@ import { JwtService } from '@nestjs/jwt';
       },
     ]),
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService, AuthService, JwtService],
+  providers: [AppService, AuthService, JwtService, UsersService],
 })
 export class AppModule {}
