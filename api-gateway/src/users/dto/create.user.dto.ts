da803cea5d3734a3a2250fec6253dd5ed50c4d31
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-30 09:53:12 +0200

    add class-validator to create user dto

--- api-gateway/src/users/dto/create.user.dto.ts
+++ api-gateway/src/users/dto/create.user.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateUserDto {
-  readonly email: string;
+  @IsString()
+  email: string;
+
+  @IsString()
   readonly password: string;
 }
