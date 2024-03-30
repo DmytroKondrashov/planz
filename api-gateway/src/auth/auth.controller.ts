ff42eefe778c24e9df5173458bc674e6d9e8385d
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-30 10:15:22 +0200

    add signin action

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/common/dto/create.user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  @Post()
-  async createUser(@Body() body: any) {
-    await this.authService.createUser(body);
+  @Post('/signin')
+  async signIn(@Body() body: CreateUserDto) {
+    await this.authService.signIn(body);
   }
 }
