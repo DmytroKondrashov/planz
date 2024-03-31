2340007cedb1e4090fc6a07f2e2e8ed0a391b238
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-31 10:52:55 +0300

    inject list model

--- api-gateway/src/lists/lists.service.ts
+++ api-gateway/src/lists/lists.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { List } from './schemas/plan.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ListsService {}
+export class ListsService {
+  constructor(@InjectModel(List.name) private listModel: Model<List>) {}
+
+  async getUsersLists(token: string) {}
+}
