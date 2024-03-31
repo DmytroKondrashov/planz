082449de0b9d7bdba99e4cc8ae1357ab38b8be21
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-31 10:46:28 +0300

    add getUsersLists action

--- api-gateway/src/lists/lists.controller.ts
+++ api-gateway/src/lists/lists.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ListsService } from './lists.service';
+import { Token } from 'src/common/decorators/token.decorator';
 
 @Controller('lists')
-export class ListsController {}
+export class ListsController {
+  constructor(private readonly listsService: ListsService) {}
+
+  @Get()
+  getUsersLists(@Token() token: string) {
+    return this.listsService.getUsersLists(token);
+  }
+}
