91cadfaeb6d551e2696d12d3001092a18115af3f
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-03-31 11:12:37 +0300

    import CommonService

--- api-gateway/src/lists/lists.module.ts
+++ api-gateway/src/lists/lists.module.ts
@@ -3,12 +3,13 @@ import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { List, ListSchema } from './schemas/plan.schema';
+import { CommonService } from 'src/common/common.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: List.name, schema: ListSchema }]),
   ],
   controllers: [ListsController],
-  providers: [ListsService],
+  providers: [ListsService, CommonService],
 })
 export class ListsModule {}
--- api-gateway/src/lists/lists.service.ts
+++ api-gateway/src/lists/lists.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { List } from './schemas/plan.schema';
 import { Model } from 'mongoose';
+import { CommonService } from 'src/common/common.service';
 
 @Injectable()
 export class ListsService {
-  constructor(@InjectModel(List.name) private listModel: Model<List>) {}
+  constructor(
+    @InjectModel(List.name) private readonly listModel: Model<List>,
+    private readonly commonService: CommonService
+  ) {}
 
-  async getUsersLists(token: string) {}
+  async getUsersLists(token: string) {
+    return this.listModel.find({ _id:  }).exec();
+  }
 }
