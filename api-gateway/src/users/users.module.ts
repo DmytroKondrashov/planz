b2d4b0a6b39823bd4d111f3ec00fbac574441ecc
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-01 16:03:32 +0300

    add CommonService import

--- api-gateway/src/users/users.module.ts
+++ api-gateway/src/users/users.module.ts
@@ -3,12 +3,13 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../common/schemas/user.schema';
+import { CommonService } from 'src/common/common.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, CommonService],
 })
 export class UsersModule {}
