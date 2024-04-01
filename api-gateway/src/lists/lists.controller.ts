79eb8e4280d74036da9bf67f8d2b5bc52f2a1908
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-01 12:49:41 +0300

    finish addList functionality

--- api-gateway/src/lists/lists.controller.ts
+++ api-gateway/src/lists/lists.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { Token } from 'src/common/decorators/token.decorator';
 import { AuthGuard } from 'src/common/guards/auth.guard';
+import { CreateListDto } from './dto/create.list.dto';
 
 @Controller('lists')
 export class ListsController {
@@ -17,5 +18,11 @@ export class ListsController {
   @UseGuards(AuthGuard)
   getList(@Token() token: string, @Param('id') id: string) {
     return this.listsService.getList(token, id);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createList(@Token() token: string, @Body() body: CreateListDto) {
+    return this.listsService.createList(token, body);
   }
 }
--- api-gateway/src/lists/lists.service.ts
+++ api-gateway/src/lists/lists.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { List } from './schemas/plan.schema';
 import { Model } from 'mongoose';
 import { CommonService } from 'src/common/common.service';
+import { CreateListDto } from './dto/create.list.dto';
 
 @Injectable()
 export class ListsService {
@@ -19,5 +20,13 @@ export class ListsService {
 
   async getList(token: string, id: string) {
     return this.listModel.find({ _id: id }).exec();
+  }
+
+  async createList(token: string, body: CreateListDto) {
+    const userId = this.commonService.getIdFromToken(token);
+    return this.listModel.create({
+      name: body.name,
+      userId,
+    });
   }
 }
