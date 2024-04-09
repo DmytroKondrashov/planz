303e89b2cdc0260338ea85842334b82c80cdb10d
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-09 21:04:20 +0300

    add List model

--- api-gateway/src/users/users.module.ts
+++ api-gateway/src/users/users.module.ts
@@ -5,10 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../common/schemas/user.schema';
 import { CommonService } from 'src/common/common.service';
 import { ListsService } from 'src/lists/lists.service';
+import { List, ListSchema } from 'src/lists/schemas/list.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: List.name, schema: ListSchema }]),
   ],
   controllers: [UsersController],
   providers: [UsersService, CommonService, ListsService],
