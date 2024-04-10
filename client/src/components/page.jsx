8c58ab968afdf584fbe78be2a75f9a65a0ad4765
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-10 14:43:33 +0300

    reqrite Page to class

--- client/src/components/page.jsx
+++ client/src/components/page.jsx
@@ -1,9 +1,58 @@
+import React from 'react';
 import LogInForm from './logInForm';
 
-function Page() {
-  return(
-    < LogInForm />
-  )
-};
+export default class Page extends React.Component{
+  addAssociation = (associationType, associationAttributes = {}) => {
+    const { intakeData } = this.state;
 
-export default Page;
+    const postData = {
+      id: intakeData.get('id'),
+      association_type: associationType,
+      association_attributes: associationAttributes
+    };
+
+    fetch(`${this.apiPrefix}/intake/${intakeData.get('project_id')}/add_association`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+      },
+      body: JSON.stringify(postData)
+    }).then(res => res.json())
+      .then(
+        (result) => {
+          console.log('============================')
+          console.log(result)
+          console.log(associationAttributes)
+          console.log('============================')
+          const updatedAssoc = {
+            id: result.assoc_id,
+            ...associationAttributes
+          };
+
+          if (intakeData.get(associationType)) {
+            const newAssocIndex = intakeData.get(associationType).size;
+            this.updateIntakeData(
+              [associationType, newAssocIndex],
+              Map(updatedAssoc)
+            )
+          } else {
+            this.updateIntakeData(
+              [associationType],
+              [Map(updatedAssoc)]
+            )
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+
+    return true;
+  }
+  render() {
+    return(
+      < LogInForm />
+    )
+  }
+};
