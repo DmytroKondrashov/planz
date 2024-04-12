656b90a517f8f9f8f1629c7114241c5517198525
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-12 12:23:29 +0300

    fix styles

--- client/src/components/logInForm.jsx
+++ client/src/components/logInForm.jsx
@@ -40,8 +40,8 @@ function LogInForm() {
 
   return(
     <>
-      <div className='row justify-content-center'>
-        <div className='col-12 col-md-auto max-width-50'>
+      <div className='row justify-content-center align-items-center mt-5'>
+        <div className='col-6 col-xl-auto'>
           <form>
             <div className="mb-3">
               <label htmlFor="email1" className="form-label">Email address</label>
@@ -51,7 +51,9 @@ function LogInForm() {
               <label htmlFor="password" className="form-label">Password</label>
               <input type="password" className="form-control" id="password" value={formData.password} onChange={handleChange} name='password'/>
             </div>
-            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+            </div>
           </form>
         </div>
       </div>
