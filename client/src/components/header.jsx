1cf3bbe891819872d9605498293f6faf402a9b91
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-14 11:10:00 +0300

    add sign up and some styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,12 +4,19 @@ import Nav from 'react-bootstrap/Nav';
 
 function Header() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" className='w-100'>
       <Navbar.Brand href="#home">PLANZ</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Lists</Nav.Link>
-        <Nav.Link href="#pricing">Sign in</Nav.Link>
+      <Nav className='w-100'>
+        <div className="mr-auto d-flex justify-content-between w-100">
+          <div className='d-flex flex-row'>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Lists</Nav.Link>
+          </div>
+          <div className='d-flex flex-row'>
+            <Nav.Link href="#pricing">Sign in</Nav.Link>
+            <Nav.Link href="#pricing">Sign up</Nav.Link>
+          </div>
+        </div>
       </Nav>
     </Navbar>
   );
