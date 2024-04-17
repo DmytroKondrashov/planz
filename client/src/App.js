35113fd7354e3791355deef04aa41757076aad23
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-17 10:25:44 +0300

    export routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RouterProvider from 'react-router-dom'
+import { routes } from './router/index'
 import Header from './components/Header';
 import Page from './components/Page';
 
