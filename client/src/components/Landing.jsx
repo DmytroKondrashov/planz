bd270107e935e7efa92c91c77d80b67bdf9f829e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-17 10:45:52 +0300

    create landing component

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,7 @@
+function Landing() {
+  return(
+    <h1 className="w-100 text-center">Welcome to the Planz app!</h1>
+  );
+}
+
+export default Landing;
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
+import Landing from "../components/Landing";
 
 export const routes = createBrowserRouter([
   {
     element: <Layout/>,
-    children: [],
+    children: [
+      {
+        path: '/',
+        element: <Landing/>
+      }
+    ],
   }
 ]);
