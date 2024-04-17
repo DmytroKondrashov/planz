037396703144c9a9c281988bffde60319d544b2b
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-17 11:25:55 +0300

    implement landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RouterProvider from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { routes } from './router/index'
 
 function App() {
