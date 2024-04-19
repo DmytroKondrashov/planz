d41a1d382c806dcd09ffa23ffbd2bc266b29c181
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-19 17:56:56 +0300

    implement SignInForm

--- client/src/components/SignUpOrLogIn.jsx
+++ client/src/components/SignUpOrLogIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
 function SignUpOrLogIn() {
   const [form, setForm] = useState(undefined);
@@ -9,7 +10,7 @@ function SignUpOrLogIn() {
       <h5 className="w-100 mt-5 text-center">Please <button className="btn btn-primary" onClick={() => setForm('signUp')}>sign up</button> or <button className="btn btn-secondary" onClick={() => setForm('signIn')}>log in</button></h5>
       <div className="mt-5">
         {form === 'signUp' && <SignUpForm/>}
-        {form === 'signIn' && <h4 className="w-100 text-center">Log in form is currently in development!</h4>}
+        {form === 'signIn' && <SignInForm/>}
       </div>
     </>
   )
