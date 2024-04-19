7def0b1d1a48edfda61b148ebb9f35d99807ace0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-19 20:36:30 +0300

    add log out link

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { useAuth } from '../hooks/AuthProvider';
 
 function Header() {
+  const { token } = useAuth();
+
   return (
     <Navbar bg="dark" variant="dark" className='w-100'>
       <Navbar.Brand href="/">PLANZ</Navbar.Brand>
@@ -12,8 +15,14 @@ function Header() {
             <Nav.Link href="#lists">Lists</Nav.Link>
           </div>
           <div className='d-flex flex-row'>
-            <Nav.Link href="#pricing">Sign in</Nav.Link>
-            <Nav.Link href="/signup">Sign up</Nav.Link>
+            {token ? (
+              <Nav.Link href="#pricing">Log Out</Nav.Link>
+            ) : (
+              <>
+                <Nav.Link href="#pricing">Sign In</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+              </>
+            )}
           </div>
         </div>
       </Nav>
