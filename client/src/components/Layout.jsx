772595c6b986aca7e9465a8f2e758f9cb204fe1d
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-19 16:53:15 +0300

    implement basic root auth protection

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,13 +1,29 @@
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
 import AuthProvider from '../hooks/AuthProvider';
+import { useAuth } from '../hooks/AuthProvider';
+import SignUpForm from './SignUpForm';
 
 function Layout() {
+  const user = useAuth();
+
   return(
     <>
       <AuthProvider>
-        <Header />
-        <Outlet />
+        {
+          user?.token ? (
+            <>
+            <Header />
+            <Outlet />
+            </>
+          ) : (
+            <>
+              <Header />
+              <h3 className='w-100 mt-5 text-center'>Please log in</h3>
+              <SignUpForm />
+            </>
+          ) 
+        }
       </AuthProvider>
     </>
   )
