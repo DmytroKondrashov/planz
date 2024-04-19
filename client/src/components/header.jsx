89f452fa96224a677d08ed1dd32410ca17ca0712
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-19 15:42:59 +0300

    return href

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,15 +5,15 @@ import Nav from 'react-bootstrap/Nav';
 function Header() {
   return (
     <Navbar bg="dark" variant="dark" className='w-100'>
-      <Navbar.Brand to="/">PLANZ</Navbar.Brand>
+      <Navbar.Brand href="/">PLANZ</Navbar.Brand>
       <Nav className='w-100'>
         <div className="mr-auto d-flex justify-content-between w-100">
           <div className='d-flex flex-row'>
-            <Nav.Link to="#lists">Lists</Nav.Link>
+            <Nav.Link href="#lists">Lists</Nav.Link>
           </div>
           <div className='d-flex flex-row'>
-            <Nav.Link to="#pricing">Sign in</Nav.Link>
-            <Nav.Link to="/signup">Sign up</Nav.Link>
+            <Nav.Link href="#pricing">Sign in</Nav.Link>
+            <Nav.Link href="/signup">Sign up</Nav.Link>
           </div>
         </div>
       </Nav>
