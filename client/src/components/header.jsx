2b0687f69835111f94498e0e0f453b696127901a
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-15 21:50:07 +0300

    add real link to lists

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { useAuth } from '../hooks/AuthProvider';
+import { Link } from 'react-router-dom';
 
 function Header() {
   const { token, logOut } = useAuth();
@@ -16,7 +17,7 @@ function Header() {
       <Nav className='w-100'>
         <div className="mr-auto d-flex justify-content-between w-100">
           <div className='d-flex flex-row'>
-            <Nav.Link href="#lists">Lists</Nav.Link>
+            <Nav.Link as={Link} to='/lists'>Lists</Nav.Link>
           </div>
           <div className='d-flex flex-row'>
             {/* TODO: find the way to handle it using the context, simply token ? () : () will not work */}
