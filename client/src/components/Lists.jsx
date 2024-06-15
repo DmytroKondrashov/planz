bdc398a8a70a89a3de2ed451af44c2ecada8a507
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-15 22:21:29 +0300

    add lists loading

--- client/src/components/Lists.jsx
+++ client/src/components/Lists.jsx
@@ -1,5 +1,56 @@
-function Lists() {
-  return(<h1>YOUR LISTS HERE</h1>);
+import { useEffect, useState } from "react";
+
+async function Lists() {
+  const [lists, setLists] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        const user = localStorage.getItem("site");
+        const response =  await fetch('http://localhost:3001/lists', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorisation': `Bearer ${user}`
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        
+        const data = await response.json();
+        setLists(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fecthData();
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>YOUR LISTS HERE</h1>
+      <ul>
+        {lists.map(list => (
+          <li key={list.id}>{list.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Lists;
