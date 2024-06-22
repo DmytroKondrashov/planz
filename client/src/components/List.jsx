655e1982d860ca3229ce4eda2d7c873baf8b08cb
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-22 22:22:16 +0300

    add list request

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function List() {
+  const {id} = useParams();
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const user = localStorage.getItem('site');
+      console.log(user)
+      const response = await fetch(`http://localhost:3001/lists/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': `Bearer ${user}`
+        }
+      })
+
+      const data = await response.json();
+      setList(data);
+    }
+
+    fetchData();
+  }, [id])
+
   return(
-    <h1>This is list</h1>
+    <>
+      <h1>This is list</h1>
+    </>
   )
 }
 
