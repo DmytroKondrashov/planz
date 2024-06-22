e01110f7bad4fbf6570efd68f6fe353770ea379c
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-22 22:31:02 +0300

    add stub table

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -8,7 +8,6 @@ function List() {
   useEffect(() => {
     const fetchData = async() => {
       const user = localStorage.getItem('site');
-      console.log(user)
       const response = await fetch(`http://localhost:3001/lists/${id}`, {
         headers: {
           'Content-Type': 'application/json',
@@ -17,6 +16,7 @@ function List() {
       })
 
       const data = await response.json();
+      console.log(data)
       setList(data);
     }
 
@@ -25,7 +25,38 @@ function List() {
 
   return(
     <>
-      <h1>This is list</h1>
+      <h1 className="text-center">This is list</h1>
+      {/* TODO: in thois table there should be a list of plans */}
+      <table class="table mt-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+    </table>
     </>
   )
 }
