f283c080ecb9ab7ecb240033e0008ac3d6371bf0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-23 11:22:52 +0300

    add plan dto

---
+++ api-gateway/src/plans/dto/create.plan.dto.ts
@@ -0,0 +1,12 @@
+import { IsDate, IsString } from 'class-validator';
+
+export class CreatePlanDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  text: string;
+
+  @IsDate()
+  due: Date;
+}
