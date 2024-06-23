6b5ced9cc0632db943d5c56df145445e5e9a741e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-23 12:30:10 +0300

    fix imports

--- api-gateway/src/plans/plans.module.ts
+++ api-gateway/src/plans/plans.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PlansController } from './plans.controller';
 import { PlansService } from './plans.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Plan, PlanSchema } from './schemas/plan.schema';
+import { CommonService } from 'src/common/common.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Plan.name, schema: PlanSchema }]),
+  ],
   controllers: [PlansController],
-  providers: [PlansService]
+  providers: [PlansService, CommonService],
 })
 export class PlansModule {}
