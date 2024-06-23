f0a1e4277c8321bd25a109fa0d99f02ca95d4fbe
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-23 12:15:46 +0300

    add createPlan

--- api-gateway/src/plans/plans.controller.ts
+++ api-gateway/src/plans/plans.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, UseGuards, Get, Param } from '@nestjs/common';
+import { Controller, UseGuards, Get, Param, Post, Body } from '@nestjs/common';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { PlansService } from './plans.service';
+import { CreatePlanDto } from './dto/create.plan.dto';
 
 @Controller('plans')
 export class PlansController {
@@ -10,5 +11,11 @@ export class PlansController {
   @UseGuards(AuthGuard)
   getPlansByLists(@Param('id') id: string) {
     return this.plansService.getPlansByLists(id);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createPlan(@Body() body: CreatePlanDto) {
+    return this.plansService.createPlan(body);
   }
 }
--- api-gateway/src/plans/plans.service.ts
+++ api-gateway/src/plans/plans.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Plan } from './schemas/plan.schema';
 import { Model } from 'mongoose';
+import { CreatePlanDto } from './dto/create.plan.dto';
 
 @Injectable()
 export class PlansService {
@@ -11,5 +12,15 @@ export class PlansService {
 
   getPlansByLists(id: string) {
     return this.planModel.find({ listId: id }).exec();
+  }
+
+  createPlan(body: CreatePlanDto): Promise<Plan> {
+    const { name, text, due, planId } = body;
+    return this.planModel.create({
+      name,
+      text,
+      due,
+      planId,
+    });
   }
 }
