daac74a5dd70dd188827773a7f3424715ae5bb4f
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-23 11:58:48 +0300

    add getPlansByLists

--- api-gateway/src/plans/plans.controller.ts
+++ api-gateway/src/plans/plans.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Param } from '@nestjs/common';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { PlansService } from './plans.service';
 
 @Controller('plans')
-export class PlansController {}
+export class PlansController {
+  constructor(private readonly plansService: PlansService) {}
+
+  @Get(':id')
+  @UseGuards(AuthGuard)
+  getPlansByLists(@Param('id') id: string) {
+    return this.plansService.getPlansByLists(id);
+  }
+}
