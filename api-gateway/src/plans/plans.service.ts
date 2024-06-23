f7e02ade20d23771524d4cb25758a53cf5ae8066
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-23 11:58:41 +0300

    add getPlansByLists

--- api-gateway/src/plans/plans.service.ts
+++ api-gateway/src/plans/plans.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Plan } from './schemas/plan.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PlansService {}
+export class PlansService {
+  constructor(
+    @InjectModel(Plan.name) private readonly planModel: Model<Plan>,
+  ) {}
+
+  getPlansByLists(id: string) {
+    return this.planModel.find({ listId: id }).exec();
+  }
+}
