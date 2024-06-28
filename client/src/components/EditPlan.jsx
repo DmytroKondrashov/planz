3cc76ba8464a637f07b40872b25fb42a4bc853be
DmytroKondrashov <kondrashovd101@gmail.com>
2024-06-28 23:15:49 +0300

    move EditPlan to a separate component

---
+++ client/src/components/EditPlan.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+function EditPlan({list, setPlans}) {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const fetchPlans = async () => {
+    const user = localStorage.getItem('site');
+    const plansResponse = await fetch(`http://localhost:3001/plans/${list._id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': `Bearer ${user}`
+      }
+    });
+
+    const plans = await plansResponse.json();
+    setPlans(plans);
+  } 
+
+  const handleNameChange = (e) => {
+    const { value } = e.target;
+    setName(value);
+  }
+
+  const handleTextChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = localStorage.getItem('site');
+      const response = await fetch('http://localhost:3001/plans', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': `Bearer ${user}`
+        },
+        body: JSON.stringify({ 
+          name,
+          text,
+          due: selectedDate,
+          listId: list._id
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok!')
+      }
+      await fetchPlans();
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  return(
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Plan Name</label>
+        <input type="name" className="form-control" id="name" value={name} onChange={handleNameChange} name='name'/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Plan Text</label>
+        <input type="text" className="form-control" id="text" value={text} onChange={handleTextChange} name='text'/>
+      </div>
+      <div className="mb-3">
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          showTimeSelect
+          dateFormat="Pp"
+          placeholderText="Select a date and time"
+        />
+      </div>
+      <div className="text-center">
+        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Create Plan</button>
+      </div>
+    </form>
+  )
+}
+
+export default EditPlan;
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,30 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import DatePicker from 'react-datepicker';
+import EditPlan from "./EditPlan";
 import 'react-datepicker/dist/react-datepicker.css';
 
 function List() {
   const {id} = useParams();
   const [list, setList] = useState(null);
   const [plans, setPlans] = useState([]);
-  const [name, setName] = useState('');
-  const [text, setText] = useState('');
-  const [errors, setErrors] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  const handleNameChange = (e) => {
-    const { value } = e.target;
-    setName(value);
-  }
-
-  const handleTextChange = (e) => {
-    const { value } = e.target;
-    setText(value);
-  }
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  }
 
   const handleDelete = async(id) => {
     const user = localStorage.getItem('site');
@@ -50,34 +32,6 @@ function List() {
     const plans = await plansResponse.json();
     setPlans(plans);
   } 
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const user = localStorage.getItem('site');
-      const response = await fetch('http://localhost:3001/plans', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'authorization': `Bearer ${user}`
-        },
-        body: JSON.stringify({ 
-          name,
-          text,
-          due: selectedDate,
-          listId: list._id
-        })
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok!')
-      }
-      await fetchPlans();
-    } catch (error) {
-      setErrors(...error, error.message);
-    }
-  }
 
   useEffect(() => {
     const user = localStorage.getItem('site');
@@ -108,28 +62,7 @@ function List() {
       <h1 className="text-center">This is list</h1>
       <>
           <div className='col-xl-auto d-flex justify-content-center'>
-            <form>
-              <div className="mb-3">
-                <label htmlFor="name" className="form-label">Plan Name</label>
-                <input type="name" className="form-control" id="name" value={name} onChange={handleNameChange} name='name'/>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="name" className="form-label">Plan Text</label>
-                <input type="text" className="form-control" id="text" value={text} onChange={handleTextChange} name='text'/>
-              </div>
-              <div className="mb-3">
-                <DatePicker
-                  selected={selectedDate}
-                  onChange={handleDateChange}
-                  showTimeSelect
-                  dateFormat="Pp"
-                  placeholderText="Select a date and time"
-                />
-              </div>
-              <div className="text-center">
-                <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Create Plan</button>
-              </div>
-            </form>
+            <EditPlan list={list} setPlans={setPlans}/>
           </div>
       </>
       {plans.length ? <>
