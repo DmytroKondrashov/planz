a17b875bb909a74ea6306ee13805b6119a7cb7e8
DmytroKondrashov <kondrashovd101@gmail.com>
2024-07-03 21:56:47 +0300

    add date picker

---
+++ client/src/components/EditPlan.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import DatePicker from 'react-datepicker';
+
+function EditPlan() {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  }
+
+  return(
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Plan Name</label>
+        <input type="name" className="form-control" value={name}/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="text" className="form-label">Plan Text</label>
+        <input type="text" className="form-control" value={text}/>
+      </div>
+      <div className="mb-3">
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          showTimeSelect
+          dateFormat="Pp"
+          placeholderText="Select a date and time"
+        />
+      </div>
+    </form>
+  )
+}
+
+export default EditPlan;
